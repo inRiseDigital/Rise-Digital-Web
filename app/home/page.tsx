b56bcadebeccbfeb815380cd910fd45588d472e2
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-02-19 16:32:36 +0530

    add nav

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,42 +5,46 @@ import Globe from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Clients from './components/Clients/Clients';
 import Tools from "./components/Tools/Tools"
+import Nav from '../components/Nav/Nav';
 
 export default function Home() {
   
   return (
-    <div className=' '>
-      <div className='relative z-50'>
-        <div className='absolute'>
+    <>
+      <div className=' '>
+        <div className='relative z-50'>
+          <div className='absolute'>
+          </div>
         </div>
-      </div>
-      <div>
-        <div className='overflow-hidden'>
-          <div className='hero-section px-3 '>
-            <Hero/>
+        <div>
+          <div className='overflow-hidden'>
+            <div className='hero-section px-3 '>
+              <Hero/>
+            </div>
+            <div id='tools' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
+              <Tools />
+            </div>
+            <div id='tools' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
+            </div>
+            <div id='clients' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
+              <Clients />
+            </div>
+            <div id='collaboration' className='home-campaign-productivity px-4 pt-8  overflow-hidden'>
+            </div>
+            <div id='productivity' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
+              <Capabilities/>
+            </div>
+            <div id='collaboration' className='home-campaign-productivity px-4 pt-8  overflow-hidden'>
+            </div>
+            <div id='security' className='home-campaign-productivity px-4 pt-8 pb-16 overflow-hidden'>
+              <Expertise/>
+            </div>
+          <Globe/>
           </div>
-          <div id='tools' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
-            <Tools />
-          </div>
-          <div id='tools' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
-          </div>
-          <div id='clients' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
-            <Clients />
-          </div>
-          <div id='collaboration' className='home-campaign-productivity px-4 pt-8  overflow-hidden'>
-          </div>
-          <div id='productivity' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
-            <Capabilities/>
-          </div>
-          <div id='collaboration' className='home-campaign-productivity px-4 pt-8  overflow-hidden'>
-          </div>
-          <div id='security' className='home-campaign-productivity px-4 pt-8 pb-16 overflow-hidden'>
-            <Expertise/>
-          </div>
-         <Globe/>
         </div>
+        <Footer/>
       </div>
-      <Footer/>
-    </div>
+      <Nav />
+    </>
   );
 }
